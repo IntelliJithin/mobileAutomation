c6b230b26c75e75ab0dec0f8b5d5e581ebb71e43
Jithin Ravindran <jravindr@emeal.nttdata.com>
2025-02-21 08:33:08 +0100

    updated file

--- wdio.conf.js
+++ wdio.conf.js
@@ -59,7 +59,7 @@ export const config = {
     capabilities: [{
         // capabilities for local Appium web tests on an Android Emulator
         'appium:platformName': 'Android',
-        'appium:platformVersion': '15.0',
+        'appium:platformVersion': '10.0',
         'appium:deviceName': 'Pixel 9',
         'appium:automationName': 'UIAutomator2',
         'appium:app': path.join(process.cwd(),'app/android/ApiDemos-debug.apk'),
