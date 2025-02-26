884b4b548846c9dae2ee60baa4ab914e96b531ac
Jithin Ravindran <jravindr@emeal.nttdata.com>
2025-02-26 12:07:50 +0100

    updated file

--- app/android/test/specs/basicTests.js
+++ app/android/test/specs/basicTests.js
@@ -3,14 +3,14 @@ import {$, browser} from "@wdio/globals";
 describe('Input text', () => {
     it('Click Views', async() => {
         const viewsElement = $('~Views');
-        await viewsElement.waitForExist({timeout:50000});
+        await viewsElement.waitForExist({timeout:50000, interval: 500});
         await viewsElement.waitForDisplayed({timeout:50000});
         await viewsElement.click();
     });
 
     it('Click Auto Complete', async() => {
         const autoCompleteElement = $('~Auto Complete');
-        await autoCompleteElement.waitForExist({timeout:50000});
+        await autoCompleteElement.waitForExist({timeout:50000, interval: 500});
         await autoCompleteElement.waitForDisplayed({timeout:50000});
         await autoCompleteElement.click();
     });
