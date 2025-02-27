fdbc01db5e5037cbea72cbc27d4348476c2bc9f2
Jithin Ravindran <jravindr@emeal.nttdata.com>
2025-02-27 10:24:29 +0100

    updated file

--- app/android/test/specs/basicTests.js
+++ app/android/test/specs/basicTests.js
@@ -1,29 +1,8 @@
 import {$, browser} from "@wdio/globals";
 
-async function handleANRDialogs() {
-    try{
-        const waitButton = $('android:id/aerr_wait');
-        const closeButton = $('android:id/aerr_close');
-
-        if (await waitButton.isDisplayed()){
-            console.log("ANR detected! Clicking 'Wait'...")
-            await waitButton.click();
-        }
-        else if (await closeButton.isDisplayed()){
-            console.log("ANR detected! Warning: 'Close app' is visible");
-            throw new Error ("ANR detected! 'Close app' is visible");
-        } else{
-            console.log("No ANR dialog detected");
-        }
-    }catch (error){
-        console.log("Error handling ANR dialogs:", error);
-    }
-}
-
 describe('Input text', () => {
     it('Click Views', async() => {
         try {
-        await handleANRDialogs();
         const viewsElement = $('~Views');
         await viewsElement.waitForExist({timeout:50000, interval: 500});
         await viewsElement.waitForDisplayed({timeout:50000});
@@ -37,7 +16,6 @@ describe('Input text', () => {
 
     it('Click Auto Complete', async() => {
         try{
-        await handleANRDialogs();
         const autoCompleteElement = $('~Auto Complete');
         await autoCompleteElement.waitForExist({timeout:50000, interval: 500});
         await autoCompleteElement.waitForDisplayed({timeout:50000});
