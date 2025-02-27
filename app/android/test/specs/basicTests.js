e492d1d3d26220ee91efb9d6baa1280e28288c6d
Jithin Ravindran <jravindr@emeal.nttdata.com>
2025-02-27 13:11:21 +0100

    updated file

--- app/android/test/specs/basicTests.js
+++ app/android/test/specs/basicTests.js
@@ -1,23 +1,29 @@
 import {$, browser} from "@wdio/globals";
 
 async function handleANRDialogs() {
-    try{
+    try {
         const waitButton = $('~Wait');
+        await waitButton.waitForExist({timeout: 5000, interval: 500});
+        await waitButton.waitForDisplayed({timeout: 5000, interval: 500});
+
+        console.log("Wait button detected:", await waitButton.isDisplayed());
 
         if (await waitButton.isDisplayed()){
             console.log("ANR detected! Clicking 'Wait'...")
             await waitButton.waitForClickable();
             await waitButton.click();
-        }
-        else{
+        } else{
             console.log("No ANR dialog detected");
         }
-    }catch (error){
+    } catch (error){
         console.log("Error handling ANR dialogs:", error);
     }
 }
 
 describe('Input text', () => {
+
+    console.log("Test suite started...");
+
     it('Click Views', async() => {
 
         beforeEach(async () => {
@@ -26,6 +32,7 @@ describe('Input text', () => {
         try {
         await handleANRDialogs();
         const viewsElement = $('~Views');
+        console.log("Checking if Views element exist...")
         await viewsElement.waitForExist({timeout:50000, interval: 500});
         await viewsElement.waitForDisplayed({timeout:50000});
         await viewsElement.click();
@@ -39,6 +46,7 @@ describe('Input text', () => {
     it('Click Auto Complete', async() => {
         try{
         const autoCompleteElement = $('~Auto Complete');
+        console.log("Checking if Auto Complete element exist...")
         await autoCompleteElement.waitForExist({timeout:50000, interval: 500});
         await autoCompleteElement.waitForDisplayed({timeout:50000});
         await autoCompleteElement.click();
