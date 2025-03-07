b66ff28cc348ef6e0d8699789088e8e183d0dd09
Jithin Ravindran <jravindr@emeal.nttdata.com>
2025-03-07 13:07:09 +0100

    updated file

--- app/android/test/specs/basicTests.js
+++ app/android/test/specs/basicTests.js
@@ -1,30 +1,5 @@
 import {$, browser} from "@wdio/globals";
 
-/*async function scrollUntilElementFound(locator, maxScrolls = 5) {
-    let scrolls = 0;
-
-    while (scrolls < maxScrolls) {
-        logToFile(`Scrolling to find element.....Attempt ${scrolls + 1}`);
-
-        try {
-            const element = $(`android=new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().description("${locator}"))`);
-            
-            if (await element.isDisplayed()){
-                logToFile("ELement found")
-                return element;
-            }
-
-        } catch (error) {
-            logToFile(`Error during scrolling attemp ${scrolls + 1}: ${error}`);
-        }
-
-        await browser.pause(1000);
-        scrolls+=1;
-    }
-    throw new Error(`Element not found after ${maxScrolls} scroll attempts`);
-    
-} */
-
 describe('Basic test', () => {
 
     it('Click Content', async() => {
@@ -32,42 +7,17 @@ describe('Basic test', () => {
         try {
             const contentElemet = $('~Content');
             await browser.pause(2500);
+            await browser.saveScreenshot('./screenshots/screen1.png')
             contentElemet.click();
             await browser.pause(2500);
+            await browser.saveScreenshot('./screenshots/screen2.png')
         } catch (error) {
 
-            await browser.saveScreenshot('./screenshots/error.png');
+            await browser.saveScreenshot('./screenshots/error_screen.png');
             throw error;
         }
 
         await browser.pause(2500);
     });
-
-   /* it('Click Auto Complete', async() => {
-
-        logToFile("Starting Auto Complete test");
-
-        try{
-
-        logToFile("Looking for auto complete element");
-
-        const autoCompleteElement = $('~Auto Complete');
-        await autoCompleteElement.waitForExist({timeout:50000});
-        await autoCompleteElement.waitForDisplayed({timeout:50000});
-
-        logToFile("clicking auto complete");
-
-        await autoCompleteElement.click();
-
-        } catch (error) {
-
-            logToFile("Error clicking auto complete: " + error.message);
-
-            await browser.saveScreenshot('./screenshots/error_Screenshot_auto_complete.png');
-            throw error;
-        }
-        await browser.pause(5000);
-    }); */
-
 
 });
