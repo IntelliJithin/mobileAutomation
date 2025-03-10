c0138331955684aee23fc631c4b7b6aaed82b19f
Jithin Ravindran <jravindr@emeal.nttdata.com>
2025-03-10 08:39:47 +0100

    updated file

--- app/android/test/specs/basicTests.js
+++ app/android/test/specs/basicTests.js
@@ -1,23 +1,29 @@
-import {$, browser} from "@wdio/globals";
-
-describe('Basic test', () => {
-
-    it('Click Content', async() => {
-
-        try {
-            const contentElemet = $('~Content');
-            await browser.pause(2500);
-            await browser.saveScreenshot('./screenshots/screen1.png')
-            contentElemet.click();
-            await browser.pause(2500);
-            await browser.saveScreenshot('./screenshots/screen2.png')
-        } catch (error) {
-
-            await browser.saveScreenshot('./screenshots/error_screen.png');
-            throw error;
-        }
-
-        await browser.pause(2500);
-    });
-
+import { $, browser } from "@wdio/globals";  
+ 
+describe('Basic test', () => {  
+    it('Click Content', async () => {  
+        try {  
+            const contentElement = await $('~Content');  
+            await browser.pause(2500);  
+            await browser.saveScreenshot('./screenshots/screen1.png');  
+ 
+            if (await contentElement.isExisting()) {  
+await contentElement.click();  
+                console.log("Successfully clicked on 'Content' element.");  
+            } else {  
+                console.warn("WARNING: 'Content' element not found. Skipping click action.");  
+                await browser.saveScreenshot('./screenshots/missing_element.png');  
+            }  
+ 
+            await browser.pause(2500);  
+            await browser.saveScreenshot('./screenshots/screen2.png');  
+ 
+        } catch (error) {  
+            console.error("ERROR: An unexpected issue occurred:", error);  
+            await browser.saveScreenshot('./screenshots/error_screen.png');  
+            throw error;  
+        }  
+        
+        await browser.pause(2500);  
+    });  
 });
