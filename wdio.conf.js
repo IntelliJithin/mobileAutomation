5e9e8a08d21e09106c41b9e49a291a5421b87d51
Jithin Ravindran <jravindr@emeal.nttdata.com>
2025-03-10 13:55:01 +0100

    updated file

--- wdio.conf.js
+++ wdio.conf.js
@@ -5,6 +5,10 @@ export const config = {
     user : process.env.BROWSERSTACK_USERNAME,
     key : process.env.BROWSERSTACK_ACCESS_KEY,
 
+    hostname : 'hub.browserstack.com',
+    protocol : 'https',
+    port : 443,
+
     specs: [
         // ToDo: define location for spec files here
         './app/android/test/specs/**/*.js'
